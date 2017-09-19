bf288c10f1de15d1581621cfd2e94fd48f46ce4c
David James <davidj28827@gmail.com>
2017-09-19 10:26:10 +1000

    Imported Wordpress Gatsby plugin to allow for API content

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,16 +4,28 @@ module.exports = {
     subtitle: `Front End Developer`
   },
   plugins: [
+    {
+      // Pull wordpress content from API
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'dfjames.com',
+        protocol: 'http',
+        hostingWPCOM: false,
+        useACF: false,
+        auth: {
+          // If auth.user and auth.pass are filled, then the source plugin will be allowed
+          // to access endpoints that are protected with .htaccess.
+          htaccess_user: 'your-htaccess-username',
+          htaccess_pass: 'your-htaccess-password',
+          htaccess_sendImmediately: false,
+        },
+        // Set verboseOutput to true to display a verbose output on `npm run develop` or `npm run build`
+        // It can help you debug specific API Endpoints problems
+        verboseOutput: false,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        path: `${__dirname}/src/pages`,
-        name: 'pages',
-      }
-    },
-    `gatsby-transformer-remark`,
     {
     resolve: `gatsby-plugin-typography`,
     options: {
