91ef19dc0e9cc2c83fb3a3674231f92deb636683
David James <davidj28827@gmail.com>
2017-09-24 16:25:09 +1000

    Pull homepage content from WP

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,24 +4,23 @@ import Link from 'gatsby-link'
 //
 // Home page of portfolio site
 //
-const IndexPage = () => (
-  <div>
-    <h1>Front End Developer</h1>
-    <h2>Located in Melbourne & passionate about all things web!</h2>
-    <p>
-      When it comes to building websites or building any form of web
-      application, I take a carefully planned approach ensuring that all facets
-      are developed with diligence. Aspects such as browser compatibility,
-      rigorous testing, code quality and performance standards are all important
-      to me.
-    </p>
-    <p>
-      Below are some of the projects I have worked on as the primary developer
-      both at Chromatix Digital Agency and other freelance work. International
-      Internships was one of my biggest achievements last year as it took out
-      the 2016 National Winner in Education at the Australian Web Awards.
-    </p>
-  </div>
-)
+const IndexPage = ({ data }) => {
+  const homepage = data.wordpressPage
+
+  return (
+    <div className="front-page">
+      <div className="page-content" dangerouslySetInnerHTML={{ __html: homepage.content }} />
+    </div>
+  )
+}
 
 export default IndexPage
+
+// Pull the homepage content from Wordpress
+export const homePageQuery = graphql`
+query homePageQuery {
+  wordpressPage(slug: {eq: "home-page"}) {
+    content
+  }
+}
+`
