b475f0c47a08fa2c3f373763b70994067ef026e9
daviddeejjames <davidj28827@gmail.com>
2017-10-19 21:57:38 +1100

    Converted to Yarn and fixed acf issues

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,7 +4,7 @@ import Footer from '../layouts/footer'
 
 const Projects = ({ data }) => {
   const projectsPage = data.wordpressPage
-  const folioItems = projectsPage.acf.folio_items
+  const folioItems = projectsPage.acf.portfolio_items
 
   return (
     <div className="projects-page page-wrap">
@@ -12,11 +12,11 @@ const Projects = ({ data }) => {
       <div className="page-content" dangerouslySetInnerHTML={{ __html: projectsPage.content }} />
       <div className="folio-items-wrap">
         {folioItems.map(item => (
-          <div key={item.title} className="folio-item">
-            <a href={item.url} target="_blank">
-
+          <div key={item.item_title} className="folio-item">
+            <a href={item.item_url} target="_blank">
+              <div className="item-image" style={{ backgroundImage: `url(${item.item_image.url})` }}> </div>
               <div className="item-title-wrap">
-                <div className="item-title">{item.title}</div>
+                <div className="item-title">{item.item_title}</div>
               </div>
             </a>
           </div>
@@ -33,14 +33,16 @@ export default Projects
 export const projectsPageQuery = graphql`
 query projectsPageQuery {
   wordpressPage(slug: {eq: "projects"}) {
-
     id
     title
     content
     acf{
-      folio_items{
-        title
-        url
+      portfolio_items{
+        item_title
+        item_image{
+          url
+        }
+        item_url
       }
     }
   }
