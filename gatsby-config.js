9e10653b954b8de02801292b90aed87782352c6d
David James <davidj28827@gmail.com>
2017-11-12 16:21:25 +1100

    Moved API to dfjames.press

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,7 +8,7 @@ module.exports = {
       // Pull wordpress content from API
       resolve: 'gatsby-source-wordpress',
       options: {
-        baseUrl: 'wordpress.dfjames.com',
+        baseUrl: 'dfjames.press',
         protocol: 'http',
         hostingWPCOM: false,
         useACF: true,
