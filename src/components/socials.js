5f9004c90d226f9644508710abce186117e05936
David James <davidj28827@gmail.com>
2024-02-24 23:28:14 +1100

    Remove old socials and update some metadata (#5)

--- src/components/socials.js
+++ src/components/socials.js
@@ -3,17 +3,13 @@ import React from 'react'
 import {
   FaGithub,
   FaCodepen,
-  FaTwitter,
   FaLinkedin,
-  FaEnvelope,
 } from 'react-icons/fa'
 
 const Socials = () => {
   const github = 'https://github.com/daviddeejjames'
   const linkedin = 'https://au.linkedin.com/in/david-james-614521106'
   const codepen = 'https://codepen.io/daviddeejjames'
-  const twitter = 'https://twitter.com/daviddeejjames'
-  const mailto = 'mailto:davidj28827@gmail.com'
 
   return (
     <div className="Socials">
@@ -29,18 +25,8 @@ const Socials = () => {
           </a>
         </li>
         <li>
-          <a href={twitter} target="_blank">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
           <a href={linkedin} target="_blank">
             <FaLinkedin />
-          </a>
-        </li>
-        <li>
-          <a href={mailto}>
-            <FaEnvelope />
           </a>
         </li>
       </ul>
